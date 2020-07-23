1cdee8cc8e4d071516152a06708845f19d33280c
Kira <datdhvinh@gmail.com>
2020-07-23 14:25:45 +0700

    Sử dụng Spring Security

---
+++ src/main/java/demo/config/security/WebSecurityAppConfig.java
@@ -0,0 +1,37 @@
+package demo.config.security;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@EnableWebSecurity
+public class WebSecurityAppConfig extends WebSecurityConfigurerAdapter {
+    /** Phương thức Override configure(AuthenticationManagerBuilder auth)
+     * cấu hình xác thực bộ nhớ với thông tin đăng nhập và vai trò của người dùng.*/
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("123456").roles("USER")
+                .and()
+                .withUser("admin").password("123456").roles("ADMIN");
+    }
+
+    /** Phương thức Override configure(HttpSecurity http) cấu hình bảo mật
+     * dựa trên web cho tất cả các yêu cầu HTTP. Theo mặc định, nó sẽ được áp dụng
+     * cho tất cả các yêu cầu, nhưng có thể bị hạn chế bằng cách sử dụng requestMatcher()
+     * hoặc các phương thức tương tự khác.*/
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/").permitAll()
+                .and()
+                .authorizeRequests().antMatchers("/user**").hasRole("USER")
+                .and()
+                .authorizeRequests().antMatchers("/admin**").hasRole("ADMIN")
+                .and()
+                .formLogin()
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+    }
+}
---
+++ src/main/java/demo/config/security/WebSecurityAppInit.java
@@ -0,0 +1,6 @@
+package demo.config.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class WebSecurityAppInit extends AbstractSecurityWebApplicationInitializer {
+}
